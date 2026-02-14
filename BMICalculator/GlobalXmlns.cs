c1c57c12bba921f13c11c8aeb03b146b6717c78f
Maria Smith <maria.t.smith023@gmail.com>
2026-02-14 15:46:56 -0500

    initial commit

---
+++ BMICalculator/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "BMICalculator")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "BMICalculator.Pages")]
